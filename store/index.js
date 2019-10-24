ad111fce79e15d1e40d9f285d327860c14a1dcd7
Nicola <nicola.delazzari@octoplus.ch>
2019-10-24 08:52:54 +0200

    update content

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 export const state = () => ({
   bio:
-    'When I design a graphic design, the purpose that I constantly pursue is the attribution of a shape, of a configuration — through study, research, a design method — which is the most suitable to all kinds of work and artifacts. I deal with visual identity for public and private bodies, book design and communication for cultural events.',
+    'When i develop a graphic design, I am pursuing the objective of attributing a shape or a configuration - through study, research, a design method - which is the most suitable to all kinds of projects and artifacts. I focus on visual communication for private and public organizations: from brand and corporate design to publishing & print media, visual identities for exhibitions and cultural events.',
   menu: [
     {
       label: '<span>A</span>b<span></span><span>o</span><span>u</span>t m<span>e</span>',
